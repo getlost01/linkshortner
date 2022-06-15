248f8056d7b2d8941f613190915ab30f7be63b7d
Aagam Jain <aagamjain573@gmail.com>
2022-06-15 17:21:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const shortId = require('shortid')
+const createHttpError = require('http-errors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const path = require('path')
+const ShortUrl = require('./models/url.model')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true ,useUnifiedTopology: true});
+    const connection=mongoose.connection;
+    try{
+        connection.once('open',()=>{
+        console.log('Database connected.')
+    })
+    }
+    catch(e){
+        console.log("Connection Fails.")
+    }
+
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res, next) => {
+  res.render('index')
+})
+
+app.post('/', async (req, res, next) => {
+  try {
+    const { url } = req.body
+    if (!url) {
+      throw createHttpError.BadRequest('Provide a valid url')
+    }
+    const urlExists = await ShortUrl.findOne({ url })
+    if (urlExists) {
+      res.render('index', {
+        short_url: `${req.headers.host}/${urlExists.shortId}`,
+      })
+      return
+    }
+    const shortUrl = new ShortUrl({ url: url, shortId: shortId.generate() })
+    const result = await shortUrl.save()
+    res.render('index', {
+      short_url: `${req.headers.host}/${result.shortId}`,
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/:shortId', async (req, res, next) => {
+  try {
+    const { shortId } = req.params
+    const result = await ShortUrl.findOne({ shortId })
+    if (!result) {
+      throw createHttpError.NotFound('Short url does not exist')
+    }
+    res.redirect(result.url)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((req, res, next) => {
+  next(createHttpError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('index', { error: err.message })
+})
+
+app.listen(3000, () => console.log('🌏 on port 3000...'))
