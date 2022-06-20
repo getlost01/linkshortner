5368ac2c908fe2cfff723c9b9d083e902c7af3be
Aagam Jain <aagamjain573@gmail.com>
2022-06-20 20:06:36 +0530

    Add files via upload

--- models/url.model.js
+++ models/url.model.js
@@ -4,12 +4,40 @@ const Schema = mongoose.Schema
 const ShortUrlSchema = new Schema({
   url: {
     type: String,
-    required: true,
+    required: true
   },
   shortId: {
     type: String,
-    required: true,
+    required: true
   },
+  custom: {
+    type: Boolean,
+    required: false
+  },
+  count: {
+    type: Number, 
+    default: 0,
+    required: true
+  },
+  TimeCreation: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
+  TimeDeletion: {
+    type: Date,
+    required: true
+  },
+  password:{
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  passwordValue: {
+    type: String,
+    default: "",
+    required: false
+  }
 })
 
 const ShortUrl = mongoose.model('shortUrl', ShortUrlSchema)
